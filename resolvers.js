4bcd8ac903bddb7381120d8a1b49a9ca3e381b53
mgbld <112904350+mgbld@users.noreply.github.com>
2023-06-16 11:14:25 -0600

    Implements fetching data from sqlite

--- resolvers.js
+++ resolvers.js
@@ -1,20 +1,7 @@
+import { getJobs } from './db/jobs.js';
+
 export const resolvers = {
   Query: {
-    jobs: () => {
-      return [
-        {
-          id: 'test-id-1', // NULLABLE
-          title: 'the Title 1',
-          description: 'The description.',
-        },
-        {
-          id: 'test-id-2', // NULLABLE
-          title: 'the Title 2',
-          description: 'The description.',
-          extraProp: 'hello', // If not part of the squema it gets ignored
-        },
-        // null,
-      ];
-    },
+    jobs: () => getJobs(),
   },
 };
